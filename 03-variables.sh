2b0d3b472bf0100b52e6fcbe6370e6143056bbfc
joindevops <info@joindevops.com>
2025-01-19 19:22:33 +0530

    some-message

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+#declaring of variable
 PERSON1=teja #no space between = and value 
 PERSON2=teju
 
+#referring variable
 echo "$PERSON1:: Hello $PERSON2, How are you?"
 echo "$PERSON2:: Hi $PERSON1, I am fine. How are you?"
 echo "$PERSON1:: I am fine too. how is your work?"
