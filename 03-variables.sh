be0f813b679937085f83d67bdb51479a77d4c63b
joindevops <info@joindevops.com>
2025-01-19 19:19:40 +0530

    some-message

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON1=chandu #no space between = and value 
-PERSON2=chaithu
+PERSON1=teja #no space between = and value 
+PERSON2=teju
 
 echo "$PERSON1:: Hello $PERSON2, How are you?"
 echo "$PERSON2:: Hi $PERSON1, I am fine. How are you?"
