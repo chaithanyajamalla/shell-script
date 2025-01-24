d6cd4a190e5447c5f951270afa144b1c30cb0a34
joindevops <info@joindevops.com>
2025-01-24 12:50:41 +0530

    hello script

--- 10-installation.sh
+++ 10-installation.sh
@@ -14,8 +14,10 @@ dnf install mysql -y
 
 if [ $? -ne 0 ]
 then
-    echo "Installation of mysqll...FAILURE"
+    echo "Installation of mysql...FAILURE"
     exit 1
+else 
+    echo "Installation of mysql...SUCCESS"
 fi
 
 dnf install git -y
@@ -24,5 +26,7 @@ if [ $? -ne 0 ]
 then
     echo "Installation of git...FAILURE"
     exit 1
+else
+    echo "Installation of git...SUCCESS"
 fi
 echo "is script proceeding?"
