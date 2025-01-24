a6b0b03f35cb4971eee861eb0271b73d51745138
joindevops <info@joindevops.com>
2025-01-24 10:44:01 +0530

    hello script

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -2,9 +2,9 @@
 
 NUMBER=$1
 
-if [ $NUMBER -gt 10 ]
+if[$NUMBER -gt 10]
 then
     echo "Given number $NUMBER is greater than 10"
 else
     echo "Given number $NUMBER is less than 10"
-fi     
+fi
